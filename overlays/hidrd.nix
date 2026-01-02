7d0ff272173419dddcf716b2d718530635f4c729
A <vars.primaryEmail>
2026-01-02 23:35:07 +0000

    adding surface pro config to nix flake

--- overlays/hidrd.nix
+++ overlays/hidrd.nix
@@ -1,6 +1,13 @@
-final: prev: {
-  hidrd = prev.hidrd.overrideAttrs (old: {
-    NIX_CFLAGS_COMPILE =
-      (old.NIX_CFLAGS_COMPILE or "") + " -Wno-error=unterminated-string-initialization";
+self: super: {
+  hidrd = super.hidrd.overrideAttrs (old: {
+    patches = [
+      (self.writeText "fix-hex-array.patch" ''
+        --- a/lib/util/hex.c
+        +++ b/lib/util/hex.c
+        @@
+        -    static const char   map[16] = "0123456789ABCDEF";
+        +    static const char   map[17] = "0123456789ABCDEF";
+      '')
+    ];
   });
 }
