82d76e0b5545b82b3fef51b547bb571517e93014
A <vars.primaryEmail>
2026-01-02 21:01:18 +0000

    adding surface pro config to nix flake

--- hosts/nixos/surface/default.nix
+++ hosts/nixos/surface/default.nix
@@ -1,12 +1,19 @@
+# hosts/nixos/surface/default.nix
 {
   config,
-  lib,
   pkgs,
+  inputs,
+  self,
+  lib,
   ...
 }:
 
 {
-  imports = [ ../../modules/kappa/hardware/thermald/thermald.nix ];
+  imports = [
+    ./hardware-configuration.nix
+    ./bootstrap.nix
+    inputs.nixos-hardware.nixosModules.microsoft-surface-pro-intel
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -50,13 +57,41 @@
     wayland
   ];
 
-  # Use wpa_supplicant in iso but not for full install
-  networking.wireless.enable = lib.mkDefault true;
-  networking.networkmanager.enable = lib.mkDefault false;
-
   hardware.microsoft-surface.kernelVersion = "stable";
 
   # Disable the problematic suspend kernel module, it makes waking up
   # impossible after closing the cover.
   boot.blacklistedKernelModules = [ "surface_gpe" ];
+
+  system.extraSystemBuilderCmds = ''
+    ln -s ${self} $out/flake
+    ln -s ${config.boot.kernelPackages.kernel.dev} $out/kernel-dev
+  '';
+
+  nix.registry = {
+    nixpkgs.flake = inputs.nixos-unstable;
+    current.to = {
+      type = "path";
+      path = "/run/booted-system/flake/";
+    };
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish.enable = true;
+  };
+
+  environment.variables = {
+    # This ensures TouchOSC can find libavahi-compat-libdnssd.so at runtime
+    LD_LIBRARY_PATH = "${pkgs.avahi-compat}/lib";
+  };
+
+  # Declare both to override base config for iso
+  networking = {
+    hostName = "surface";
+    wireless.enable = true;
+    networkmanager.enable = false;
+  };
+
 }
