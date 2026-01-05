90367a355ea3571ac35e1bd0d6d3a03192cee18b
A <adam@coopr.network>
2026-01-05 23:12:58 +0000

    troubleshooting niri wm

--- hosts/nixos/surface/modules/kappa/software/niri.nix
+++ hosts/nixos/surface/modules/kappa/software/niri.nix
@@ -4,7 +4,7 @@
   ...
 }:
 {
-  imports = [ inputs.niri.nixosModules.niri ];
+  # imports = [ inputs.niri.nixosModules.niri ];
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri-unstable;
 
