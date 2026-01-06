df9060dd5adf1913a0eda9beac87c889ee05c301
A <adam@coopr.network>
2026-01-06 22:14:36 +0000

    adding shared modules

--- modules/common/fonts.nix
+++ modules/common/fonts.nix
@@ -20,22 +20,11 @@ in
     fonts = {
       packages = with pkgs; [
         maple-mono.NF
-        #  dejavu_fonts
         fira-code
-        #  hack-font
-        #  inconsolata
-        #  source-code-pro
-        #  ubuntu_font_family
-        #  nerd-fonts.fira-code
         nerd-fonts.jetbrains-mono
-        #  nerd-fonts.fantasque-sans-mono
         nerd-fonts.iosevka
-        #  nerd-fonts.sauce-code-pro
         nerd-fonts.victor-mono
         nerd-fonts.symbols-only
-        #  nerd-fonts._0xproto
-        #  nerd-fonts.droid-sans-mono
-        #  nerd-fonts.zed-mono
       ];
     };
   };
